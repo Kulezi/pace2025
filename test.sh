7deb3997b344c0c559b3b20f6a70f14ba855a7d6
Paweł Putra <pawelputra0@gmail.com>
2024-11-23 20:46:25 +0100

    add tiny test set provided by PACE

--- test.sh
+++ test.sh
@@ -1,15 +1,16 @@
-for i in $(seq 1 1000); do
-    echo $i
-    ./rg.out >rg.gr
-    ./brute.out <rg.gr | head -n 1 >brute.ans
-    ./main.out <rg.gr | head -n 1 >main.ans
-    diff brute.ans main.ans || exit 1
-done
+# for i in $(seq 1 1000); do
+#     echo $i
+#     ./rg.out >rg.gr
+#     ./brute.out <rg.gr | head -n 1 >brute.ans
+#     ./main.out <rg.gr | head -n 1 >main.ans
+#     diff brute.ans main.ans || exit 1
+# done
 
-for test in in/tw/*; do
+for test in in/tiny/testset/*; do
     echo "Running $test:"
-    timeout 30 ./main.out <$test >ds.ans
+    timeout 600 ./main.out <$test >ds.ans
     ./out_verifier.out $test ds.ans
 done
 
 make clean
+rm rg.gr
