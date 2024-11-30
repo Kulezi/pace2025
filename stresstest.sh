5075fd0a8bed210a7a80d1ff63527c357b3b1bb9
Paweł Putra <pawelputra0@gmail.com>
2024-11-30 11:06:25 +0100

    stresstest: stop on runtime errors

--- stresstest.sh
+++ stresstest.sh
@@ -1,14 +1,39 @@
 for i in $(seq 1 1000); do
     printf "\nRunning test $i\n"
+    
+    # Generate random input
     ./rg.out >rg.gr
+    if [ $? -ne 0 ]; then
+        echo "Runtime error in rg.out"
+        exit 1
+    fi
+
+    # Run brute force solution
     ./brute.out <rg.gr | head -n 1 >brute.ans
-    ./main.out <rg.gr | head -n 1 >main.ans
+    if [ $? -ne 0 ]; then
+        echo "Runtime error in brute.out"
+        cat rg.gr
+        exit 1
+    fi
+
+    # Run main solution
+    ./main.out <rg.gr 2>main.log | head -n 1 >main.ans 
+    if [ $? -ne 0 ]; then
+        echo "Runtime error in main.out"
+        cat rg.gr
+        cat main.log
+        exit 1
+    fi
+
+    # Compare outputs
     diff brute.ans main.ans
     if [ $? -ne 0 ]; then
+        echo "Mismatch found!"
         cat rg.gr
+        cat main.log
         exit 1
     fi
 done
 
-echo "[OK] Stresstest done"
+echo "[OK] Stress test done"
 make clean
