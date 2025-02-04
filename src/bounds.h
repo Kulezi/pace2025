61698a16fae934472996cf837c9e0d179d6b98af
Paweł Putra <pawelputra0@gmail.com>
2025-02-04 17:22:39 +0100

    bounds: fix upper_bound

--- src/bounds.h
+++ src/bounds.h
@@ -8,28 +8,31 @@ namespace bounds {
 
 // Returns an approximation that is a upper bound on the number of additional vertices needed to
 // make this instance fully dominated, calculated by a greedy algorithm always taking the maximum
-// degree undominated node first. Time complexity: O (n * log m)
+// degree undominated node first. Time complexity: O ((n + m) * log(n+m))
 int greedy_upper_bound(const Instance &g) {
     int upper_bound = 0;
     auto nodes = g.nodes;
     std::priority_queue<std::pair<int, int>> pq;
-    std::vector<int> deg(g.next_free_id, 0);
+    std::vector<int> undominated_neighbors(g.next_free_id, 0);
     for (auto u : nodes) {
-        if (g.getStatus(u) == UNDOMINATED) deg[u]++;
+        if (g.getStatus(u) == UNDOMINATED) undominated_neighbors[u]++;
         for (auto v : g.neighbourhoodExcluding(u))
-            if (g.getStatus(v) == UNDOMINATED) deg[u]++;
+            if (g.getStatus(v) == UNDOMINATED) undominated_neighbors[u]++;
 
-        if (g.getStatus(u) != TAKEN) pq.push({deg[u], u});
+        if (g.getStatus(u) != TAKEN && undominated_neighbors[u] > 0)
+            pq.push({undominated_neighbors[u], u});
     }
 
     auto status = g.status;
     auto dominate = [&](int u) {
         if (status[u] == UNDOMINATED) {
             status[u] = DOMINATED;
+            --undominated_neighbors[u];
+            if (undominated_neighbors[u] > 0) pq.push({undominated_neighbors[u], u});
             for (auto v : g.neighbourhoodExcluding(u)) {
                 if (status[v] == TAKEN) continue;
-                deg[v]--;
-                pq.push({deg[v], v});
+                --undominated_neighbors[v];
+                if (undominated_neighbors[v] > 0) pq.push({undominated_neighbors[v], v});
             }
         }
     };
@@ -38,14 +41,12 @@ int greedy_upper_bound(const Instance &g) {
         auto [d, v] = pq.top();
         pq.pop();
         // This might not be the minimum degree node anymore.
-        if (d != deg[v] || status[v] == TAKEN) continue;
+        if (d > undominated_neighbors[v] || status[v] == TAKEN) continue;
 
-        if (status[v] == UNDOMINATED) {
-            for (auto u : g.neighbourhoodExcluding(v)) dominate(u);
-        }
-
-        upper_bound++;
+        dominate(v);
         status[v] = TAKEN;
+        for (auto u : g.neighbourhoodExcluding(v)) dominate(u);
+        ++upper_bound;
     }
 
     return upper_bound;
@@ -66,6 +67,6 @@ int lower_bound(const Instance &g) {
 // Returns an approximation that is a upper bound on the number of additional vertices needed to
 // make this instance fully dominated.
 int upper_bound(const Instance &g) { return greedy_upper_bound(g); }
-}  // namespace
+}  // namespace bounds
 
 #endif  // BOUNDS_H
