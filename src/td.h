8dfb11210a6b5f4a4b0ee95dcef5f825deec467c
Paweł Putra <pawelputra0@gmail.com>
2025-03-06 18:22:20 +0100

    drop htd dependency

--- src/td.h
+++ src/td.h
@@ -3,8 +3,6 @@
 
 #include <chrono>
 #include <csignal>
-#include <htd/main.hpp>
-#include <htd_io/main.hpp>
 #include <iostream>
 #include <memory>
 #include <string>
@@ -12,26 +10,6 @@
 #include "instance.h"
 #include "rrules.h"
 
-class FitnessFunction : public htd::ITreeDecompositionFitnessFunction {
-   public:
-    FitnessFunction(void) {}
-    ~FitnessFunction() {}
-    htd::FitnessEvaluation *fitness(const htd::IMultiHypergraph &graph,
-                                    const htd::ITreeDecomposition &decomposition) const {
-        HTD_UNUSED(graph)
-        /**
-         * Here we specify the fitness evaluation for a given decomposition.
-         * In this case, we select the maximum bag size and the height.
-         */
-        return new htd::FitnessEvaluation(2, -(double)(decomposition.maximumBagSize()),
-                                          -(double)(decomposition.height()));
-    }
-
-    FitnessFunction *clone(void) const { return new FitnessFunction(); }
-};
-
-std::unique_ptr<htd::LibraryInstance> manager(htd::createManagementInstance(htd::Id::FIRST));
-enum class HtdNodeType { Introduce, Leaf, Forget, Join };
 enum class NodeType { IntroduceVertex, IntroduceEdge, Leaf, Forget, Join };
 struct DecompositionNode {
     int id;
@@ -50,72 +28,17 @@ struct DecompositionNode {
 struct TreeDecomposition {
    public:
     TreeDecomposition(Instance &g) : g(g) {
-        loadGraph(g);
-        htdDecompose();
-        // Rewrite the decomposition so it fits the definition from Platypus Book.
-        makeDecomposition();
-        delete htd_decomposition;
-        delete graph;
+
     };
 
     DecompositionNode &operator[](int v) { return decomp[v]; }
     int root;
 
    private:
-    htd::IMutableGraph *graph;
     std::vector<int> reverse_mapping;
-    htd::ITreeDecomposition *htd_decomposition;
+
     Instance &g;
     std::vector<DecompositionNode> decomp;
-
-    void loadGraph(Instance &g) {
-        // Dummy value for 1-indexing.
-        reverse_mapping = {0};
-
-        int N = 0;
-        std::map<int, htd::vertex_t> mapping;
-        for (auto v : g.nodes) {
-            mapping[v] = ++N;
-            reverse_mapping.push_back(v);
-        }
-
-        graph = manager->graphFactory().createInstance(N);
-        for (auto u : g.nodes)
-            for (auto v : g.adj[u])
-                if (u < v) graph->addEdge(mapping[u], mapping[v]);
-    }
-
-    void htdDecompose() {
-        FitnessFunction f;
-        htd::TreeDecompositionOptimizationOperation *operation =
-            new htd::TreeDecompositionOptimizationOperation(manager.get(), f.clone());
-
-        operation->setManagementInstance(manager.get());
-        operation->setVertexSelectionStrategy(new htd::RandomVertexSelectionStrategy(10));
-        operation->addManipulationOperation(new htd::NormalizationOperation(manager.get()));
-
-        manager->orderingAlgorithmFactory().setConstructionTemplate(
-            new htd::MinDegreeOrderingAlgorithm(manager.get()));
-
-        htd::ITreeDecompositionAlgorithm *baseAlgorithm =
-            manager->treeDecompositionAlgorithmFactory().createInstance();
-
-        baseAlgorithm->addManipulationOperation(operation);
-        htd::IterativeImprovementTreeDecompositionAlgorithm algorithm(manager.get(), baseAlgorithm,
-                                                                      f.clone());
-
-        algorithm.setIterationCount(1);
-        algorithm.setNonImprovementLimit(0);
-        htd_decomposition = algorithm.computeDecomposition(*graph);
-    }
-
-    HtdNodeType getNodeType(htd::vertex_t v) {
-        if (htd_decomposition->isForgetNode(v)) return HtdNodeType::Forget;
-        if (htd_decomposition->isLeaf(v)) return HtdNodeType::Leaf;
-        if (htd_decomposition->isIntroduceNode(v)) return HtdNodeType::Introduce;
-        if (htd_decomposition->isJoinNode(v)) return HtdNodeType::Join;
-        throw std::logic_error("found decomposition node of unknown type");
-    }
 
     int createNode(NodeType type, std::vector<int> bag, int v = -1, int to = -1, int lChild = -1,
                    int rChild = -1) {
@@ -132,95 +55,7 @@ struct TreeDecomposition {
         return decomp.back().id;
     }
 
-    int makeDecompositionNode(htd::vertex_t htd_id) {
-        HtdNodeType type = getNodeType(htd_id);
-        std::vector<int> htd_bag = std::vector<int>(htd_decomposition->bagContent(htd_id).begin(),
-                                                    htd_decomposition->bagContent(htd_id).end());
-        for (auto &v : htd_bag) v = reverse_mapping[v];
 
-        switch (type) {
-            case HtdNodeType::Introduce: {
-                assert(htd_decomposition->introducedVertexCount(htd_id) == 1);
-
-                int lChild = makeDecompositionNode(htd_decomposition->childAtPosition(htd_id, 0));
-                int v = reverse_mapping[htd_decomposition->introducedVertexAtPosition(htd_id, 0)];
-
-                return createNode(NodeType::IntroduceVertex, htd_bag, v, -1, lChild);
-            }
-            case HtdNodeType::Forget: {
-                assert(htd_decomposition->forgottenVertexCount(htd_id) == 1);
-
-                int lChild = makeDecompositionNode(htd_decomposition->childAtPosition(htd_id, 0));
-                int v = reverse_mapping[htd_decomposition->forgottenVertexAtPosition(htd_id, 0)];
-
-                // For each disappearing edge insert an IntroduceEdge node between Forget and its
-                // direct child.
-                for (auto to : htd_bag) {
-                    if (g.hasEdge(v, to)) {
-                        lChild =
-                            createNode(NodeType::IntroduceEdge, decomp[lChild].bag, v, to, lChild);
-                    }
-                }
-
-                return createNode(NodeType::Forget, htd_bag, v, -1, lChild);
-            }
-
-            case HtdNodeType::Leaf: {
-                assert(htd_decomposition->childCount(htd_id) == 0);
-
-                // htd provides a decomposition where leaves can have nonempty bags,
-                // so replace a bag of size n with n introduces ending with a leaf.
-                std::vector<int> bag;
-                int last = createNode(NodeType::Leaf, {});
-
-                for (auto i : htd_bag) {
-                    bag.push_back(i);
-                    last = createNode(NodeType::IntroduceVertex, bag, i, -1, last);
-                }
-
-                return last;
-            }
-            case HtdNodeType::Join: {
-                assert(htd_decomposition->childCount(htd_id) == 2);
-
-                int htd_l_child = htd_decomposition->childAtPosition(htd_id, 0);
-                int htd_r_child = htd_decomposition->childAtPosition(htd_id, 1);
-
-                int l_child = makeDecompositionNode(htd_l_child);
-                int r_child = makeDecompositionNode(htd_r_child);
-
-                return createNode(NodeType::Join, htd_bag, -1, -1, l_child, r_child);
-            }
-
-            default:
-                throw std::logic_error("unknown HtdNodeType encountered");
-        }
-    }
-
-    // Returns the id of the root of decomposition.
-    void makeDecomposition() {
-        int htd_root = htd_decomposition->root();
-        root = makeDecompositionNode(htd_root);
-
-        // The root bag might not be empty yet, so we forget vertices one by one, possibly
-        // introducing new edges.
-        while (!decomp[root].bag.empty()) {
-            auto new_bag = decomp[root].bag;
-            int forgotten = new_bag.back();
-            new_bag.pop_back();
-
-            // For each disappearing edge insert an IntroduceEdge node between Forget and its
-            // direct child.
-            for (auto to : new_bag) {
-                if (g.hasEdge(forgotten, to)) {
-                    root =
-                        createNode(NodeType::IntroduceEdge, decomp[root].bag, forgotten, to, root);
-                }
-            }
-
-            root = createNode(NodeType::Forget, new_bag, forgotten, -1, root);
-        }
-    }
 
     void printDecomp(int v, int level) {
         std::cerr << std::string(level, ' ');
