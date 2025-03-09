8d19f3f3a3f2aec9a61ed4ac3338509d91ccac6b
Paweł Putra <pawelputra0@gmail.com>
2025-03-09 15:45:24 +0100

    eval_all.sh: fix header line

--- eval_all.sh
+++ eval_all.sh
@@ -11,7 +11,10 @@ mkdir $SOLUTIONS
 
 basename -a in/PACE2025-instances/ds/exact/*.gr | parallel -j2 "./eval.sh {}"
 
-echo -ne "file" 
+
+echoerr() { echo "$@" 1>&2; }
+
+echoerr -ne "file" 
 echo "p ds 0 0" | ./eval.out >/dev/null
 ls $SOLUTIONS/*.content | xargs cat
 
