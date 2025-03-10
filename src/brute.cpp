3a99e9b3a52a84df789668449406fd4f23527a8e
Paweł Putra <pawelputra0@gmail.com>
2025-03-10 11:18:09 +0100

    ds.h: keep bruteforce clean of reductions

--- src/brute.cpp
+++ src/brute.cpp
@@ -4,7 +4,7 @@
 #include "instance.h"
 #include "rrules.h"
 int main() {
-    DomSet::Exact ds(RRules::defaults_preprocess, RRules::defaults_preprocess);
+    DomSet::Exact ds({}, {});
 
     Instance g(std::cin);
     auto sol = ds.solveBruteforce(g, std::cout);
