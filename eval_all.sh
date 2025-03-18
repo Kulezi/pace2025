30a88bd4efafecadd4c6ea015ae9485848e1ceab
Paweł Putra <pawelputra0@gmail.com>
2025-03-18 15:11:29 +0100

    eval: refactor scripts

--- eval_all.sh
+++ eval_all.sh
@@ -2,6 +2,8 @@
 
 INSTANCES="in/PACE2025-instances/ds/exact"
 SOLUTIONS="${INSTANCES}/.solutions"
+N_CORES=$(nproc --all)
+TIME_LIMIT_SECONDS=60
 
 cmake .
 make eval
@@ -9,12 +11,9 @@ make eval
 rm -rf $SOLUTIONS
 mkdir $SOLUTIONS
 
-basename -a in/PACE2025-instances/ds/exact/*.gr | parallel --halt never -j2 "./eval.sh {}" || true
+basename -a in/PACE2025-instances/ds/exact/*.gr | parallel --halt never -j${N_CORES} "./eval.sh {} $TIME_LIMIT_SECONDS" || true
 
-
-echoerr() { echo "$@" 1>&2; }
-
-echoerr -ne "file" 
+echo -ne "file" 
 echo "p ds 0 0" | ./eval.out $SOLUTIONS/empty.gr >/dev/null
 ls $SOLUTIONS/*.content | xargs cat
 
