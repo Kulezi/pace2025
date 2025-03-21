91c2e717942a71f432acfeef247d229cd90ee6f2
Paweł Putra <pawelputra0@gmail.com>
2025-03-21 13:48:41 +0100

    eval.sh: change timeout signal to INT, print memory and time usage

--- eval.sh
+++ eval.sh
@@ -7,14 +7,23 @@ fi
 INSTANCES="in/PACE2025-instances/ds/exact"
 SOLUTIONS="${INSTANCES}/.solutions"
 TIMEOUT_SECONDS=$2
+TIME_OUTPUT=$SOLUTIONS/$1.time
+TIMEOUT="timeout -s INT $TIMEOUT_SECONDS"
+TIME="/usr/bin/time --quiet -f seconds=%e\nkbytes=%M -o $TIME_OUTPUT"
 
 echo "Running test $1"
 
 CONTENT=${SOLUTIONS}/$1.content
+TEMP_CONTENT=$(mktemp)
 HEADER=${SOLUTIONS}/$1.header
 
-echo -ne $1 > $CONTENT
-echo -ne file > $HEADER
-timeout $TIMEOUT_SECONDS ./eval.out ${SOLUTIONS}/$1 < ${INSTANCES}/$1 >> $CONTENT 2>> $HEADER
-printf "\n" >>  $CONTENT
-printf "\n" >>  $HEADER
+printf "file,time(s),memory(MB)" > $HEADER
+printf $1 > $CONTENT
+$TIMEOUT $TIME ./eval.out ${SOLUTIONS}/$1 < ${INSTANCES}/$1 >> $TEMP_CONTENT 2>> $HEADER
+
+source $TIME_OUTPUT
+printf "\n" >> $HEADER
+printf ",${seconds},$(expr ${kbytes} / 1024)" >> $CONTENT
+
+cat $TEMP_CONTENT >> $CONTENT
+printf "\n" >> $CONTENT
