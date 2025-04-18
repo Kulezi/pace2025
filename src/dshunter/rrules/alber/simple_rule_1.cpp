8822974a0331a8f150989d448223a46b81f8b25b
Paweł Putra <pawelputra0@gmail.com>
2025-04-18 18:05:08 +0200

    rrules: handle disregarded nodes in AlberSimpleRule1

--- src/dshunter/rrules/alber/simple_rule_1.cpp
+++ src/dshunter/rrules/alber/simple_rule_1.cpp
@@ -1,21 +1,28 @@
 #include "../rrules.h"
 namespace DSHunter {
 bool alberSimpleRule1(Instance& g) {
-    std::vector<std::pair<int, int>> to_remove;
     for (auto v : g.nodes) {
         for (auto [w, status] : g[v].adj) {
-            if (v > w || status == EdgeStatus::FORCED) continue;
-            if (g.isDominated(v) && g.isDominated(w)) {
-                to_remove.emplace_back(v, w);
+            if (status == EdgeStatus::UNCONSTRAINED && g.isDominated(v) && g.isDominated(w)) {
+                g.removeEdge(v, w);
+                return true;
+            }
+
+            if (g.isDisregarded(v) && g.isDisregarded(w)) {
+                DS_ASSERT(status != EdgeStatus::FORCED);
+                g.removeEdge(v, w);
+                return true;
+            }
+
+            if (status == EdgeStatus::FORCED && g.isDisregarded(v)) {
+                DS_ASSERT(!g.isDisregarded(w));
+                g.take(w);
+                return true;
             }
         }
     }
-    for (auto [v, w] : to_remove) {
-        DS_TRACE(std::cerr << "applying " << __func__ << dbg(v) << dbg(w) << std::endl);
-        g.removeEdge(v, w);
-    }
 
-    return !to_remove.empty();
+    return false;
 }
 
 ReductionRule AlberSimpleRule1("AlberSimpleRule1 (dominated edge removal)", alberSimpleRule1, 2, 1);
