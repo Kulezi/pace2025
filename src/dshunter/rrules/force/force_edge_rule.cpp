4f86094e6f7fadaab3523490a90d86850ceb3c59
Paweł Putra <p.putra@student.uw.edu.pl>
2025-05-02 14:18:21 +0200

    rrules: improve ForcedEdgeRule

--- src/dshunter/rrules/force/force_edge_rule.cpp
+++ src/dshunter/rrules/force/force_edge_rule.cpp
@@ -3,44 +3,60 @@
 namespace DSHunter {
 
 bool forceEdgeRule(Instance& g) {
-    for (auto v : g.nodes) {
+    static int guwno = 0;
+
+    auto goober = g.nodes;
+    bool ret = false;
+    for (auto v : goober) {
+        if (!std::binary_search(goober.begin(), goober.end(), v)) continue;
         if (g.deg(v) == 2 && !g.isDominated(v)) {
             auto e1 = g[v].adj[0];
             auto e2 = g[v].adj[1];
 
-            if (!g.hasEdge(e1.to, e2.to) || (g.isDisregarded(e1.to) && g.isDisregarded(e2.to)))
+            if (g.isDisregarded(e1.to) && g.isDisregarded(e2.to))
                 continue;
 
-            if (e1.status == EdgeStatus::UNCONSTRAINED && e2.status == EdgeStatus::UNCONSTRAINED) {
-                DS_TRACE(std::cerr << __func__ << "(1)" << dbg(v) << std::endl);
+            if (g.hasEdge(e1.to, e2.to)) {
+                if (e1.status == EdgeStatus::UNCONSTRAINED && e2.status == EdgeStatus::UNCONSTRAINED) {
+                    DS_TRACE(std::cerr << __func__ << "(1)" << dbg(v) << std::endl);
+                    g.removeNode(v);
+                    if (g.getEdgeStatus(e1.to, e2.to) != EdgeStatus::FORCED)
+                        g.forceEdge(e1.to, e2.to);
+                    // return true;
+                    ret = true;
+                } else if (e1.status == EdgeStatus::FORCED && e2.status == EdgeStatus::UNCONSTRAINED && !g.isDisregarded(e1.to)) {
+                    DS_TRACE(std::cerr << __func__ << "(2)" << dbg(v) << dbg(e1.to) << std::endl);
+                    // Taking e1.to is optimal, as it's always better than taking v, and we are
+                    // forced to take one of them.
+                    g.take(e1.to);
+
+                    // return true;
+                    ret = true;
+                } else if (e1.status == EdgeStatus::UNCONSTRAINED && e2.status == EdgeStatus::FORCED && !g.isDisregarded(e2.to)) {
+                    DS_TRACE(std::cerr << __func__ << "(3)" << dbg(v) << dbg(e2.to) << std::endl);
+                    // Taking e2.to is optimal, as it's always better than taking v, and we are
+                    // forced to take one of them.
+                    g.take(e2.to);
+
+                    // return true;
+                    ret = true;
+                } else {
+                    // We need to take either just u, or both e1.to and e2.to.
+                    // TODO: contract them into a single vertex thats treated the following way:
+                    // If it's taken into ds it adds e1.to and e2.to to the dominating set.
+                    // If it's not taken it adds just v to the dominating set.
+                }
+            } else if (g.isDominated(e1.to) && g.isDominated(e2.to)) {
                 g.removeNode(v);
-                if (g.getEdgeStatus(e1.to, e2.to) != EdgeStatus::FORCED)
-                    g.forceEdge(e1.to, e2.to);
-                return true;
-            } else if (e1.status == EdgeStatus::FORCED && e2.status == EdgeStatus::UNCONSTRAINED && !g.isDisregarded(e1.to)) {
-                DS_TRACE(std::cerr << __func__ << "(2)" << dbg(v) << dbg(e1.to) << std::endl);
-                // Taking e1.to is optimal, as it's always better than taking v, and we are
-                // forced to take one of them.
-                g.take(e1.to);
-
-                return true;
-            } else if (e1.status == EdgeStatus::UNCONSTRAINED && e2.status == EdgeStatus::FORCED && !g.isDisregarded(e2.to)) {
-                DS_TRACE(std::cerr << __func__ << "(3)" << dbg(v) << dbg(e2.to) << std::endl);
-                // Taking e2.to is optimal, as it's always better than taking v, and we are
-                // forced to take one of them.
-                g.take(e2.to);
-
-                return true;
-            } else {
-                // We need to take either just u, or both e1.to and e2.to.
-                // TODO: contract them into a single vertex thats treated the following way:
-                // If it's taken into ds it adds e1.to and e2.to to the dominating set.
-                // If it's not taken it adds just v to the dominating set.
+                g.addEdge(e1.to, e2.to);
+                g.forceEdge(e1.to, e2.to);
+                // return true;
+                ret = true;
             }
         }
     }
 
-    return false;
+    return ret;
 }
 
 ReductionRule ForceEdgeRule("ForceEdgeRule", forceEdgeRule, 1, 1);
