0243f23593027de10055a2b39701592c7ad3d9ce
Paweł Putra <p.putra@student.uw.edu.pl>
2025-05-13 19:34:04 +0200

    rrules: add SameDominatorsRule

--- src/dshunter/rrules/dominate/same_dominators_rule.cpp
+++ src/dshunter/rrules/dominate/same_dominators_rule.cpp
@@ -1,18 +1,32 @@
 #include "../rrules.h"
 namespace {
+bool applySameDominatorsRule(DSHunter::Instance& g, int u, int v) {
+    if (u != v && !g.isDominated(u) && !g.isDominated(v) && DSHunter::contains(g[u].dominators, g[v].dominators)) {
+        g.markDominated(u);
+        return true;
+    }
+    return false;
+}
 
 }  // namespace
 namespace DSHunter {
 
+
 bool sameDominatorsRule(Instance& g) {
     bool reduced = false;
+
+    // Iterate over all pairs (u, w) of nodes at distance at most 2.
     for (auto u : g.nodes) {
-        if ()
+        for (auto v : g[u].n_open) {
+            for (auto w : g[v].n_closed) {
+                reduced |= applySameDominatorsRule(g, u, w);
+            }
+        }
     }
 
     return reduced;
 }
 
-ReductionRule SameDominatorsRule("SameDominatorsRule", sameDominatorsRule, 2, 1);
+ReductionRule SameDominatorsRule("SameDominatorsRule", sameDominatorsRule, 3, 2);
 
 }  // namespace DSHunter
